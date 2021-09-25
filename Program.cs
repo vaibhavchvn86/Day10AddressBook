e46a933ef5026d0fbb2c15c2523eb326215a2afe
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-25 21:59:01 +0530

    solution file of Address Book

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Day10Assignment_AddressBook;
+using System;
+
+namespace Day9AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Welcome To Address Book Program****");
+            //#region UC1
+            //UC1_AddBook.Contact();
+            //#endregion
+
+            //#region UC2
+            //UC2_AddBook.AddPerson();
+            //#endregion
+
+            //#region UC3
+            //UC3_AddBook.AddBook1();
+            //#endregion
+
+            //#region UC4
+            //UC4_AddBook.AddBook2();
+            //#endregion
+
+            //#region UC5
+            //UC5_AddBook.AddBook5();
+            //#endregion
+
+            //#region UC6
+            //UC6_AddBook.AddBook6();
+            //#endregion
+            AddBook.AddPer();
+        }
+    }
+}
