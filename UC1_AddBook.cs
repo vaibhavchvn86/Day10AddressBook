b25c65a4055481e08b112a11ab47897e79e1d8fa
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-25 21:52:58 +0530

    Creating Address Book

---
+++ UC1_AddBook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9AddressBook
+{
+   public class UC1_AddBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+
+        public static List<UC1_AddBook> ContactDetails = new List<UC1_AddBook>();
+        public static void Contact()
+        {
+            UC1_AddBook person = new UC1_AddBook();
+
+            Console.Write("Enter FirstName: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter LastName: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Mobile Number: ");
+            person.MobileNumber = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode : ");
+            person.Zipcode = Console.ReadLine();
+
+            Console.Write("Enter Email : ");
+            person.Email = Console.ReadLine();
+
+            ContactDetails.Add(person);
+
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Mobile Number: " + person.MobileNumber);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.Zipcode);
+            Console.WriteLine("Email : " + person.Email);
+            Console.ReadKey();
+        }
+    }
+}
